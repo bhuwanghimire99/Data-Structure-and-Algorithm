125dda5cfa2a0d502cfd9ee597ca9e74112ebacd
bhuwanghimire99 <bhuwanghimire.bg.10@gmail.com>
2023-11-10 00:20:08 +0545

    some errors are fixed

--- Stack/balancedParanthesis.js
+++ Stack/balancedParanthesis.js
@@ -1,28 +1,32 @@
 
-const balancedParanthesis =  (val) => {
-// Create a stack
-let item = []
-// Loop through each element in the string
-for (i = 0; i < val.length; i ++) {
+const balancedParanthesis = (val) => {
+    // Create a stack
+    let item = []
+    // Loop through each element in the string
+    for (i = 0; i < val.length; i++) {
+
+
+
+        let char = item[item.length - 1] // keeps data of top stack
+
+        // If you encounter a starting bracket, push it onto the stack 
+
+        if (val[i] == "(" || val[i] == "{" || val[i] == "[") {
+            item.push(val[i])
+        }
+
+        // Pop the opening bracket off the stack,
+        // if there is a corresponding closing bracket in the string
+        else if ((char == "(" && val[i] == ")") || (char == "{" && val[i] == "}") || (char == "[" && val[i] == "]")) {
+            item.pop()
+
+        } else return false
+
+    }
+    // Check empty stack
+    if (item.length == 0) return true
+    else  return false
 }
-let char = item[item.length-1]
 
-// If you encounter a starting bracket, push it onto the stack 
-
-if (val[i] == "(" || val[i] == "{" || val[i] =="[")
-{
-    stck.push(s[i])
-}
-
-// Pop the opening bracket off the stack,
-
-// if there is a corresponding closing bracket in the string } else 
-if ((char == "(" && s[i] == ")") || (char == "{" && s[i] "}") || (char =="[" && s[i]=="]")
-
-
- {
-    stck.pop()
-} else return false
-// Check empty stack
-return stck.length? false : true
-}
+console.log(balancedParanthesis('({[}])')) // unbalanced
+console.log(balancedParanthesis('({[]})')) // balanced
