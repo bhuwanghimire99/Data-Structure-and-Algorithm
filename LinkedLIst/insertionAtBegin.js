48e8ccfe9ccfa79bda07ec1601a1d88545f8ef04
bhuwanghimire99 <bhuwanghimire.bg.10@gmail.com>
2023-11-13 00:36:33 +0545

    some issues are solved

--- LinkedLIst/insertionAtBegin.js
+++ LinkedLIst/insertionAtBegin.js
@@ -12,22 +12,19 @@ class LinkedList {
     }
 
     isEmpty() {
-        if (this.size == 0) {
-            console.log("empty linked list")
-        }
-        else return false
+       return this.size==0
     }
     getSize() {
         return this.size
     }
 
-    insertAtBeginning() {
+    insertAtBeginning(data) {
         const node = new Node(data) // Create a new node with the provided data
-        if (this.isEmpty) {
+        if (this.isEmpty()) {
             this.head = node
         }
         else {
-            node.next = this.head, // Set the next of the new node to the current head of the list
+            node.next = this.head // Set the next of the new node to the current head of the list
                 this.head = node // Update the head to be the new node, making it the first element
         }
         this.size++
@@ -35,20 +32,22 @@ class LinkedList {
     display() {
 
         let current = this.head;  // Start from the head of the list
-
-        while (current !== null) {  // Traverse the list and print each element's data
-            console.log(current.data + " ");
+      let listData= ''
+        while (current!==null) {  // Traverse the list and print each element's data
+           listData+= `${current.data} `
+    
             current = current.next;
         }
+        console.log(listData);
     }
 }
 
 // Create a new linked list
 let linkedList = new LinkedList();
 
-linkedList.insertAtBeginning(3);
-linkedList.insertAtBeginning(7);
-linkedList.insertAtBeginning(11);
+linkedList.insertAtBeginning(10);
+linkedList.insertAtBeginning(20);
+linkedList.insertAtBeginning(30);
 
 // Display the elements of the linked list
 linkedList.display();
