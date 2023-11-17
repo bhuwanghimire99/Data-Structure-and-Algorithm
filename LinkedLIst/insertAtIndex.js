ee429a2143a63424802eb8e4dfadf0380ed85106
bhuwanghimire99 <bhuwanghimire.bg.10@gmail.com>
2023-11-17 16:25:30 +0545

    adding deletion feature

--- LinkedLIst/insertAtIndex.js
+++ LinkedLIst/insertAtIndex.js
@@ -8,37 +8,73 @@ class Node {
 class insertAtIndex {
     constructor() {
         this.size = 0;
-        
+
     }
 
     isEmpty() {
         return this.size == 0
     }
 
-    insert(value,index){ // method that takes value and index 
+    insert(value, index) { // method that takes value and index 
 
-      if (index<0 || index>this.size) {
-        console.log("please input valid index");
-        
-      }
-      else if(index==0){
-        const node = new Node(value) // creating a node with given value
+        if (index < 0 || index > this.size) {
+            console.log("please input valid index");
 
-        this.head=node
-        this.size++
-      }
-      else{
-        const node = new Node(value) // creating a node with given value
+        }
+        else if (index == 0) {
+            const node = new Node(value) // creating a node with given value
 
-       let prev=this.head;
-       for(let i=0;i<index-1;i++){ // index 2 given xa vane 1 ma stop hunx 1 prev , 2 prev.next
-        prev = prev.next
-       }
-       node.next=prev.next; // index 2=prev.next , index1 = prev
-       prev.next=node;
-       this.size++
-      }
+            this.head = node
+            this.size++
+        }
+        else {
+            const node = new Node(value) // creating a node with given value
+
+            let prev = this.head;
+            for (let i = 0; i < index - 1; i++) { // index 2 given xa vane 1 ma stop hunx 1 prev , 2 prev.next
+                prev = prev.next
+            }
+            node.next = prev.next; // index 2=prev.next , index1 = prev
+            prev.next = node;
+            this.size++
+        }
     }
+
+
+    deleteFrom(index) {
+        let removedNode;
+
+        if (index < 0 || index >= this.size) {
+            console.log("please input valid index");
+
+
+        }
+        else if (index == 0) {
+            removedNode = this.head;
+            this.head = this.head.next
+            this.size--
+
+        }
+        else {
+            let prev = this.head
+            for ( let i = 0; i < this.index - 1; i++) {
+                prev = prev.next
+
+            }
+            removedNode = prev.next;
+            prev.next = removedNode.next
+            this.size--
+        }
+
+
+        return removedNode
+
+    }
+
+
+
+
+
 
     display() {
         if (this.isEmpty()) {
@@ -57,18 +93,24 @@ class insertAtIndex {
     }
 }
 
-const linkedList =new insertAtIndex();
-linkedList.insert(10,2);
-linkedList.insert(10,0);
-linkedList.insert(20,1);
-linkedList.insert(30,2);
-linkedList.insert(40,3);
+const linkedList = new insertAtIndex();
+linkedList.insert(10, 2);
+linkedList.insert(10, 0);
+linkedList.insert(20, 1);
+linkedList.insert(30, 2);
+linkedList.insert(40, 3);
 linkedList.display();
 
-linkedList.insert(35,3);
-linkedList.insert(38,4);
+linkedList.insert(35, 3);
+linkedList.insert(38, 4);
 linkedList.display();
 
+linkedList.deleteFrom(10);
 
+linkedList.deleteFrom(0);
+linkedList.display();
 
+console.log(linkedList.deleteFrom(2));
+
+linkedList.display();
 
