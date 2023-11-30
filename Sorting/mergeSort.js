5ab3800e57ff860f486bdeb5d092ccb291187518
bhuwanghimire99 <bhuwanghimire.bg.10@gmail.com>
2023-11-30 23:03:13 +0545

    merge sort algorithm implemented

---
+++ Sorting/mergeSort.js
@@ -0,0 +1,34 @@
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+      return arr;
+    }
+  
+    const mid = Math.floor(arr.length / 2); // mid as pivot
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+  
+    return merge(mergeSort(left), mergeSort(right));
+  }
+  
+  function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+  
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        result.push(left[leftIndex]);
+        leftIndex++;
+      } else {
+        result.push(right[rightIndex]);
+        rightIndex++;
+      }
+    }
+  
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+  }
+  
+  
+  const arrayToSort = [8, 3, -9, 5, 6, 4,- 2, 7];
+  const sortedArray = mergeSort(arrayToSort);
+  console.log(sortedArray);
